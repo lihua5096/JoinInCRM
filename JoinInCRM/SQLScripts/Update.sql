e75455d5affb897cf551c6f6ce1cf7efc9843a63
lihua5096 <just_do_it@qq.com>
2018-05-30 16:44:28 +0800

    add SQL Script
    ++

---
+++ JoinInCRM/SQLScripts/Update.sql
@@ -0,0 +1,12 @@
+Create table EmpImportInfo(
+ CompanyName nvarchar(100),
+ StoreName nvarchar(100),
+ EmpNo nvarchar(30),
+ EmpName nvarchar(12),
+ Sex nvarchar(2),
+ CellPhone nvarchar(15),
+ CardNo nvarchar(30),
+ [Role] nvarchar(20),
+ Leader nvarchar(10)
+ 
+)
